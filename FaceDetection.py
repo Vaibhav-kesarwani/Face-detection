4780f36fa13b5b402e71d70382fe56760df1887a
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-05-18 14:16:08 +0530

    added the detection system

--- FaceDetection.py
+++ FaceDetection.py
@@ -1,13 +1,13 @@
 import cv2
 import mediapipe as mp
 import time
-# "Videos/1.mp4"
-cap = cv2.VideoCapture(0)
+
+cap = cv2.VideoCapture("Videos/1.mp4")
 pTime = 0
 
 mpFaceDetection = mp.solutions.face_detection
 mpDraw = mp.solutions.drawing_utils
-faceDetection = mpFaceDetection.FaceDetection(0.75)
+faceDetection = mpFaceDetection.FaceDetection(0.85)
 
 while True:
     success, img = cap.read()
@@ -18,18 +18,16 @@ while True:
 
     if results.detections:
         for id, detection in enumerate(results.detections):
-            # mpDraw.draw_detection(img, detection)
+            mpDraw.draw_detection(img, detection)
             # print(id, detection)
             # print(detection.score)
             # print(detection.location_data.relative_bounding_box)
             bboxC = detection.location_data.relative_bounding_box
             ih, iw, ic = img.shape
-            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih),
-            int(bboxC.width * iw), int(bboxC.height * ih)
+            bbox = (int(bboxC.xmin * iw), int(bboxC.ymin * ih),
+                    int(bboxC.width * iw), int(bboxC.height * ih))
             cv2.rectangle(img, bbox, (255, 0, 255), 2)
-            cv2.putText(img, f'{int(detection.score[0] * 100)}%',
-           (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_PLAIN,
-            2, (255, 0, 255), 2)
+            cv2.putText(img, f'{int(detection.score[0] * 100)}%', (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
@@ -37,4 +35,4 @@ while True:
     cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN,
     3, (0, 255, 0), 2)
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    cv2.waitKey(50)
